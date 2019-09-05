ed9c4a7b986e868b5dbfbf2dacf7f2d07f18a060
Katamaranas <ernestas.zidokas@gmail.com>
2019-09-05 12:13:38 +0300

    fixed image preload

--- src/app/pages/Game/index.js
+++ src/app/pages/Game/index.js
@@ -3,11 +3,18 @@ import { SingleCard, Timer } from '../../components';
 import { connect } from 'react-redux';
 import WinScreen from '../WinScreen';
 import game from '../../../game';
+import { Helmet } from 'react-helmet';
+import * as cardImages from './cardsImages';
 import './index.scss';
 
 function Game({ cards, inGame }) {
   return (
     <div className="Game">
+      <Helmet>
+        {cards.map(({ num, suit, id }) => (
+          <link key={id} rel="preload" as="image" type="image/jpg" href={cardImages[suit + num]} />
+        ))}
+      </Helmet>
       {!inGame && <WinScreen />}
       <Timer />
       <div className="Cards">
