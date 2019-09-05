2941c1f5aebd0d675549e2be6fdc1fd8e4dddb7e
Katamaranas <ernestas.zidokas@gmail.com>
2019-09-05 20:24:06 +0300

    Fixed bug when opened card tryies to open again and buggs out

--- src/app/components/SingleCard/index.js
+++ src/app/components/SingleCard/index.js
@@ -7,6 +7,11 @@ import { cardBack } from '../../constants';
 import './index.scss';
 
 function SingleCard({ suit, num, isOpen, id, openCard, cardName, cardCount }) {
+  const onClick = () => {
+    if (!isOpen) {
+      openCard(id);
+    }
+  };
   return (
     <div className="SingleCard">
       <img
@@ -14,7 +19,7 @@ function SingleCard({ suit, num, isOpen, id, openCard, cardName, cardCount }) {
         alt={
           isOpen ? `${cardName ? cardName : num} of ${suit}` : `Closed ${cardBack[cardCount]} card`
         }
-        onClick={() => openCard(id)}
+        onClick={onClick}
       />
     </div>
   );
